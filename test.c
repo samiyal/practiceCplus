436edc23d7070b7b345e6b75dd425cb1fd113590
samiyal <40146305+samiyal@users.noreply.github.com>
2018-08-23 20:18:25 +0530

    Update test.c
    Add comment and print welcome message

--- test.c
+++ test.c
@@ -1,8 +1,17 @@
+/*  Comments
+  Program: W.AP for to print welcome message
+  Date   : 23 Aug 2018
+  Author : Mr. Sam
+  Review : Mr. Sam
+
+*/
+
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
 
+  cout << "Welcome program in c plus" << endl;
  return 0;
 }
